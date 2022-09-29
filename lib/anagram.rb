a19db5366f292c2edfe052837660dc73883f2ec4
Bambam320 <igor.mestechkin@gmail.com>
2022-09-29 02:26:57 -0400

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize word
+    @word = word
+  end
+  def match array
+    newarr = []
+    array.each do |b| 
+      if @word.chars.sort == b.chars.sort
+        newarr << b
+      end
+    end
+    return newarr
+  end
+end
